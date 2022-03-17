fc98bf8cc8681b746abf48fbc94816cb288aee70
YAPOREGIS <berganyapo@gmail.com>
2022-03-16 22:25:47 -0700

    my task 0 commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio>
+#include <stdlib>
+#include <main.h>
+gcc gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
